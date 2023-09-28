7b38f51d072cba68c9a8a2cb144205c66829e0c4
Akeops <lalanne.andoni1@gmail.com>
2023-09-28 12:52:46 +0200

    Ajout de la navbar

---
+++ pages/commons/menu.php
@@ -0,0 +1,24 @@
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="source/images/icons8-livre-64.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </a>
+    <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+  </div>
+  
+</nav>
--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,8 @@
 <?php ob_start(); ?>
 
 <h1>Page d'accueil</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-info">Info</button>
 
 <?php
     $content = ob_get_clean();
