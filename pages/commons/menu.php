0a9d5d1632540fccc3351c5a34269eec4ddec906
Akeops <lalanne.andoni1@gmail.com>
2023-10-02 19:44:42 +0200

    Navbar terminé

--- pages/commons/menu.php
+++ pages/commons/menu.php
@@ -1,22 +1,22 @@
 
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar bg-body-tertiary perso_bgBlack">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       <img src="source/images/icons8-livre-64.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       Bootstrap
     </a>
     <ul class="nav justify-content-end">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <li class="nav-item mx-2">
+    <a class="nav-link active perso_colorJaune" aria-current="page" href="#">Active</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+  <li class="nav-item ">
+    <a class="nav-link perso_colorJaune mx-2" href="#">Link</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+  <li class="nav-item perso_colorJaune">
+    <a class="nav-link perso_colorJaune mx-2" href="#">Link</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  <li class="nav-item perso_colorJaune mx-2">
+    <a class="nav-link perso_colorJaune" href="#">Disabled</a>
   </li>
 </ul>
   </div>
