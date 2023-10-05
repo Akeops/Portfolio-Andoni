195f6189f150e8ca0b4dc4dacc596606409a9ca0
Akeops <lalanne.andoni1@gmail.com>
2023-10-05 12:56:28 +0200

    navbar terminé + caroussel

--- pages/commons/menu.php
+++ pages/commons/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-xl bg-body-tertiary perso_bgBlack">
+<nav class="navbar ml-auto navbar-expand-xl  perso_bgBlack">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       <img src="../source/images/Capture d'écran 2023-10-03 234038.png" alt="Bootstrap" width="30" height="24">
@@ -7,23 +7,18 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse perso_colorJaune" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mb-2 mb-lg-0 perso_colorJaune ms-auto ml-auto">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Zelda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Mario</a>
         </li>
       </ul>
   </div>
