ec82f8b6a33ad3fde40f13b905ec3014b1eac0e7
Akeops <lalanne.andoni1@gmail.com>
2023-10-09 15:56:17 +0200

    Ajout de la première modal pour la première card

--- pages/index.php
+++ pages/index.php
@@ -67,6 +67,8 @@
     </div>
 </div>
 
+
+
 <!-- Card -->
 <div class="container text-center" id="mesProjets">
     <h2 class="m-5 perso_colorYellow">Mes projets</h2>
@@ -75,9 +77,34 @@
         <div class="card-body">
             <h5 class="card-title">Portfolio</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+             <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cours1">
+                Launch demo modal
+            </button>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="cours1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div>
+
+
+
     <div class="card" style="width: 18rem;">
         <img src="../source/images/card3.png" class="card-img-top" alt="...">
         <div class="card-body">
