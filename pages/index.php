dcbb98dc6b9cf8efc07a164229597f94584ad83e
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-10-13 18:41:05 +0200

    Modal du projet 2 ajoutée

--- pages/index.php
+++ pages/index.php
@@ -77,14 +77,14 @@
             <div class="col">
                 <div class="card" style="width: 18rem;">
                     <img src="../source/images/card1.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Portfolio</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cours1">
-                                Détails
-                            </button>
-                        </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Portfolio</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cours1">
+                            Détails
+                        </button>
+                    </div>
                 </div>
             </div>
             <div class="col">
@@ -93,7 +93,9 @@
                     <div class="card-body">
                         <h5 class="card-title">ITwatchdog</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Détails</a>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cours2">
+                            Détails
+                        </button>
                     </div>
                 </div>
             </div>
@@ -103,11 +105,15 @@
                     <div class="card-body">
                         <h5 class="card-title">Je ne sais pas encore</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cours3">
+                            Détails
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
     
 
     <!-- Modal (Fenêtre qui apparaît quand on clique sur le bouton de la card) -->
@@ -120,6 +126,44 @@
                 </div>
                 <div class="modal-body">
                     <img src="../source/images/card1.jpg" class="card-img-top" alt="...">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit nemo libero ipsa blanditiis earum unde, neque eum modi obcaecati repellendus odio quos deserunt sunt quia nobis reprehenderit! Laboriosam, optio.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal (Fenêtre qui apparaît quand on clique sur le bouton de la card) -->
+    <div class="modal fade" id="cours2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="cours2">ITwatchdog</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="../source/images/card3.png" class="card-img-top" alt="...">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit nemo libero ipsa blanditiis earum unde, neque eum modi obcaecati repellendus odio quos deserunt sunt quia nobis reprehenderit! Laboriosam, optio.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal (Fenêtre qui apparaît quand on clique sur le bouton de la card) -->
+    <div class="modal fade" id="cours3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="cours2">ITwatchdog</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="../source/images/card3.png" class="card-img-top" alt="...">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit nemo libero ipsa blanditiis earum unde, neque eum modi obcaecati repellendus odio quos deserunt sunt quia nobis reprehenderit! Laboriosam, optio.</p>
                 </div>
                 <div class="modal-footer">
