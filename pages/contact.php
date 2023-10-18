fac2e7fe63897b5c0183d6f783901e1ffdfd2aa1
Akeops <lalanne.andoni1@gmail.com>
2023-10-18 22:50:50 +0200

    Ajout d'un tableau sans bord vierge

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,24 @@
 <?php ob_start(); ?>
 
-<h1>Page de contact</h1>
+<div class="container text-center" id="presentation">
+    <h2 class="m-5 perso_colorYellow">Mes informations personnelles</h2>
+    <table class="table table-borderless">
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Jacob</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 <?php
 
