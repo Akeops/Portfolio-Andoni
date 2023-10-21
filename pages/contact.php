e0241db047174a93624eb3ab2477f317de40afa6
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-10-21 15:19:01 +0200

    Ajout du php dans la page de contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,14 @@
 <?php ob_start(); ?>
 
+<?php 
+if(isset($_POST['mail'])){
+    echo "Envoi du message: " . $_POST['mail'] . "<br>";
+    echo "Envoi du message: " . $_POST['objet'] . "<br>";
+    echo "Envoi du message: " . $_POST['message'] . "<br>";
+}
+
+?>
+
 <div class="container text-center">
     <h2 class="m-5 perso_colorYellow">Mes informations personnelles</h2>
     <table class="m-5 table table-borderless">
@@ -25,12 +34,16 @@
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="disabledSelect" class="form-label perso_colorYellow" id="objet" name="objet">Objet du mail:</label>
-            <select id="disabledSelect" class="form-select">
+            <label for="disabledSelect" class="form-label perso_colorYellow" id="objet">Objet du mail:</label>
+            <select id="disabledSelect" class="form-select" name="objet">
                 <option value="question">Question</option>
                 <option value="remarque">Remarque</option>
                 <option value="autre">Autre</option>
             </select>
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label perso_colorYellow">Message:</label>
+            <textArea class="form-text" id="message" rows="3" name="message" placeholder="Entrez votre message..."></textArea>
         </div>
         <button type="submit" class="btn btn-primary">Valider</button>
     </form>
